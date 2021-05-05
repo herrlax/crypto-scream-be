bd413e83ca4ac01299823305e0ed83f43fedd4fb
Mikael Malmqvist <mikael@malmqvist.it>
2021-05-05 16:59:22 +0200

    Add caching

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,66 @@
 import express, { Application, Request, Response } from "express";
 import fetch from "node-fetch";
+import NodeCache from "node-cache";
+
+const PORT = 9000;
+const KRAKEN_API_URL = "https://api.kraken.com/0/public/Ticker";
+const TIME_TO_LIVE = 2 * 60; // invalidate cache after 2 minutes
 
 const app: Application = express();
-const port = 9000;
-const KRAKEN_API_URL = "https://api.kraken.com/0/public/Ticker";
+const cache = new NodeCache({ stdTTL: TIME_TO_LIVE, checkperiod: 0 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const getPrice = async (currency1: string, currency2: string) => {
+  const res = await fetch(
+    `${KRAKEN_API_URL}?pair=${currency1}${currency2}&count=1`
+  );
+
+  const data = await res.json();
+
+  if (data.error.length > 0) {
+    return {
+      error: data.error[0],
+    };
+  }
+
+  return { price: data.result[Object.keys(data.result)[0]].c[0] };
+};
+
 app.get(
   "/price/:currency_1/:currency_2",
   async (req: Request, response: Response): Promise<Response> => {
-    const res = await fetch(
-      `${KRAKEN_API_URL}?pair=${req.params.currency_1}${req.params.currency_2}&count=1`
-    );
+    const currency1 = req.params.currency_1;
+    const currency2 = req.params.currency_2;
 
-    const data = await res.json();
+    const cachedValue = cache.get(`${currency1}${currency2}`);
 
-    if (data.error.length > 0) {
-      return response.status(404).send({
-        error: data.error[0],
+    if (cachedValue) {
+      return response.status(200).send({
+        data: cachedValue,
       });
     }
 
-    const priceObject = data.result[Object.keys(data.result)[0]];
+    const data = await getPrice(currency1, currency2);
+
+    if (data.error) {
+      return response.status(404).send({
+        error: data.error,
+      });
+    }
+
+    cache.set(`${currency1}${currency2}`, data.price);
 
     return response.status(200).send({
-      data: {
-        price: priceObject.c[0],
-      },
+      data: data.price,
     });
   }
 );
 
 try {
-  app.listen(port, (): void => {
-    console.info(`Listening on port ${port}`);
+  app.listen(PORT, (): void => {
+    console.info(`Listening on port ${PORT}`);
   });
 } catch (error) {
   console.error(error.message);
