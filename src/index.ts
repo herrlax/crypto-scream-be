178f2c4b85dd669912c65360c672530ab67b040d
Mikael Malmqvist <mikael@malmqvist.it>
2021-05-05 16:28:32 +0200

    Add support for getting price pair

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,30 @@ const app: Application = express();
 const port = 9000;
 const KRAKEN_API_URL = "https://api.kraken.com/0/public/Ticker";
 
-const ETH_USD = "ethusd";
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get(
-  "/price",
+  "/price/:currency_1/:currency_2",
   async (req: Request, response: Response): Promise<Response> => {
-    const res = await fetch(`${KRAKEN_API_URL}?pair=${ETH_USD}&count=1`);
+    const res = await fetch(
+      `${KRAKEN_API_URL}?pair=${req.params.currency_1}${req.params.currency_2}&count=1`
+    );
+
     const data = await res.json();
 
+    if (data.error.length > 0) {
+      return response.status(404).send({
+        error: data.error[0],
+      });
+    }
+
+    const priceObject = data.result[Object.keys(data.result)[0]];
+
     return response.status(200).send({
-      data: data.result.XETHZUSD.c[0],
+      data: {
+        price: priceObject.c[0],
+      },
     });
   }
 );
